3c9d6d9873857a4a3fb8ca1b3b0c24de57795057
soslane <soslane@163.com>
2025-10-10 10:57:54 +0800

    css update gezicss

---
+++ README.md
@@ -0,0 +1,8 @@
+# Aria Template
+
+This is a personal blog, portfolio, or blog template created for [Astro](https://astro.build).
+
+Astro port of [aria](https://github.com/static-templates/aria).
+
+![AA](https://github.com/user-attachments/assets/fa1d88a5-7e8d-4abb-a129-9cb1875498ce)
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            }
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
+};
